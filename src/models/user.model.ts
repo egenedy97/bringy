0cfb8262554fecfc81d48f287d4c5574f2795371
Eslam Genedy <eslamgenedy377@yahoo.com>
2023-07-30 03:47:33 +0300

    add user model

---
+++ src/models/user.model.ts
@@ -0,0 +1,30 @@
+import mongoose, { Schema, Document } from "mongoose";
+import bcrypt from "bcrypt";
+
+export interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+}
+
+const userSchema = new Schema<IUser>({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (value: string) => /\S+@\S+\.\S+/.test(value),
+      message: "Invalid email format",
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+export const UserModel = mongoose.model<IUser>("User", userSchema);
