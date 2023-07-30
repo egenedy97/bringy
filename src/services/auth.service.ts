c44937f4314abd6ea20cff61b58d3d5207d063ff
Eslam Genedy <eslamgenedy377@yahoo.com>
2023-07-30 03:48:07 +0300

    add userService and authService

---
+++ src/services/auth.service.ts
@@ -0,0 +1,77 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { HttpException } from "../exceptions/HttpException";
+import { DataStoredInToken, TokenData } from "../interfaces/auth.interface";
+import { User } from "../interfaces/user.interface";
+import { UserModel } from "../models/user.model";
+
+class AuthService {
+  public users = UserModel;
+
+  public async signup(userData: {
+    email: string;
+    name: string;
+    password: string;
+  }): Promise<User> {
+    console.log("test", userData);
+    const findUser: User = await this.users.findOne({ email: userData.email });
+    if (findUser)
+      throw new HttpException(
+        409,
+        `You're email ${userData.email} already exists`
+      );
+
+    const hashedPassword = await bcrypt.hash(userData.password, 10);
+    const createUserData: User = await this.users.create({
+      ...userData,
+      password: hashedPassword,
+    });
+
+    return createUserData;
+  }
+
+  public async login(userData: {
+    email: string;
+    password: string;
+  }): Promise<{ token: string; findUser: User }> {
+    const findUser: User = await this.users.findOne({ email: userData.email });
+    if (!findUser)
+      throw new HttpException(409, `You're email ${userData.email} not found`);
+
+    const isPasswordMatching: boolean = await bcrypt.compare(
+      userData.password,
+      findUser.password
+    );
+
+    if (!isPasswordMatching)
+      throw new HttpException(409, "You're password not matching");
+
+    const { token } = this.createToken(findUser);
+
+    return { token, findUser };
+  }
+
+  public async logout(userData: User): Promise<User> {
+    const findUser: User = await this.users.findOne({
+      email: userData.email,
+      password: userData.password,
+    });
+    if (!findUser)
+      throw new HttpException(409, `You're email ${userData.email} not found`);
+
+    return findUser;
+  }
+
+  public createToken(user: User): TokenData {
+    const dataStoredInToken: DataStoredInToken = { _id: user._id };
+    const secretKey: string = process.env.SECRET_KEY;
+    const expiresIn: number = 4 * 60 * 60 * 1000;
+
+    return {
+      expiresIn,
+      token: jwt.sign(dataStoredInToken, secretKey, { expiresIn }),
+    };
+  }
+}
+
+export default AuthService;
---
+++ src/services/users.service.ts
@@ -0,0 +1,78 @@
+import bcrypt from "bcrypt";
+import { HttpException } from "../exceptions/HttpException";
+import { User } from "../interfaces/user.interface";
+import { UserModel } from "../models/user.model";
+
+class UserService {
+  public users = UserModel;
+
+  public async findAllUser(): Promise<User[]> {
+    const users: User[] = await this.users.find();
+    return users;
+  }
+
+  public async findUserById(userId: string): Promise<User> {
+    const findUser: User = await this.users.findOne({ _id: userId });
+    if (!findUser) throw new HttpException(409, "You're not user");
+
+    return findUser;
+  }
+
+  public async createUser(userData: {
+    name: string;
+    email: string;
+    password: string;
+  }): Promise<User> {
+    const findUser: User = await this.users.findOne({ email: userData.email });
+    if (findUser)
+      throw new HttpException(
+        409,
+        `You're email ${userData.email} already exists`
+      );
+
+    const hashedPassword = await bcrypt.hash(userData.password, 10);
+    const createUserData: User = await this.users.create({
+      ...userData,
+      password: hashedPassword,
+    });
+
+    return createUserData;
+  }
+
+  public async updateUser(
+    userId: string,
+    userData: { name: string; email: string; password: string }
+  ): Promise<User> {
+    if (userData.email) {
+      const findUser: User = await this.users.findOne({
+        email: userData.email,
+      });
+      if (findUser && findUser._id != userId)
+        throw new HttpException(
+          409,
+          `You're email ${userData.email} already exists`
+        );
+    }
+
+    if (userData.password) {
+      const hashedPassword = await bcrypt.hash(userData.password, 10);
+      userData = { ...userData, password: hashedPassword };
+    }
+
+    const updateUserById: User = await this.users.findByIdAndUpdate(userId, {
+      userData,
+    });
+    if (!updateUserById) throw new HttpException(409, "You're not user");
+
+    return updateUserById;
+  }
+
+  public async deleteUser(userId: string): Promise<User> {
+    const deleteUserById: User = await this.users.findByIdAndDelete(userId);
+    if (!deleteUserById) throw new HttpException(409, "You're not user");
+
+    return deleteUserById;
+  }
+}
+
+export default UserService;
