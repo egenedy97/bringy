30ebccb2a9e8157437566dbee9aef2c4a53e342b
Eslam Genedy <eslamgenedy377@yahoo.com>
2023-07-30 03:48:53 +0300

    add middleware

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,42 @@
+import { NextFunction, Response } from "express";
+import jwt from "jsonwebtoken";
+import { HttpException } from "../exceptions/HttpException";
+import {
+  DataStoredInToken,
+  RequestWithUser,
+} from "../interfaces/auth.interface";
+import { UserModel } from "../models/user.model";
+
+const authMiddleware = async (
+  req: RequestWithUser,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const Authorization =
+      req.body.token || req.query.token || req.headers["x-access-token"];
+    console.log("====>", Authorization);
+    if (Authorization) {
+      const secretKey: string = process.env.SECRET_KEY;
+      const verificationResponse = (await jwt.verify(
+        Authorization,
+        secretKey
+      )) as DataStoredInToken;
+      const userId = verificationResponse._id;
+      const findUser = await UserModel.findById(userId);
+
+      if (findUser) {
+        req.user = findUser;
+        next();
+      } else {
+        next(new HttpException(401, "Wrong authentication token"));
+      }
+    } else {
+      next(new HttpException(404, "Authentication token missing"));
+    }
+  } catch (error) {
+    next(new HttpException(401, "Wrong authentication token"));
+  }
+};
+
+export default authMiddleware;
